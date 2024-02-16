026d49a26c142fdabe5d7a83377ef75c010ba15f
timothyyppan <timothypan676@gmail.com>
2024-02-15 22:29:15 -0500

    stuff

--- DateToEpoch.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def dates_to_epochs(datetime_column):
-    
-    datetime_column = pd.to_datetime(datetime_column)
-    epoch = pd.Timestamp('1970-01-01')
-    datetime_column = (datetime_column - epoch) // pd.Timedelta('1s')
-    
-    return datetime_column
