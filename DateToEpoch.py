bbae39903f0cec08fd06da3716b7865f24f04618
timothyyppan <timothypan676@gmail.com>
2024-02-15 21:50:19 -0500

    Update DateToEpoch.py

--- DateToEpoch.py
+++ DateToEpoch.py
@@ -5,3 +5,5 @@ def datetime_to_epoch(datetime):
     if pd.isnull(datetime):
         return None
     date_format = "%Y-%m-%d %H:%M"
+    parsed_datetime = datetime.strptime(datetime, date_format)
+    
