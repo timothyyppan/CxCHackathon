cf90cf124dc5a01aa68cd6fbe02586b7f77721c2
SunYeet <101126284+SunYeet@users.noreply.github.com>
2024-02-15 22:29:50 -0500

    fire cause not working sadge

---
+++ test.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from datetime import datetime
+import ImpactScore as imsc
+import FireCause as fc
+import numpy as np
+
+excel_file = 'fp-historical-wildfire-data-2006-2021.xlsx'
+
+df = pd.read_excel(excel_file, engine='openpyxl')
+
+#df['impact_score'] = df.apply(imsc.get_impact_score(df), axis=1)
+
+#df['fire_cause'] = df.apply(fc.filter_fire_causes, axis=1)
+df_filtered_causes = fc.filter_fire_causes(df)
+print(df_filtered_causes['general_cause_desc'].iloc[90, 91])
+#indices = [90, 91, 92, 93, 94, 95, 96]
+#print(df_filtered_causes.iloc[indices])
