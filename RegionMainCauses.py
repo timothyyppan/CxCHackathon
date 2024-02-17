53e47e2e9e44d9581684c8e1939746a23e16941d
SunYeet <101126284+SunYeet@users.noreply.github.com>
2024-02-16 23:20:37 -0500

    region causes started

--- RegionMainCauses.py
+++ RegionMainCauses.py
@@ -1,4 +1,25 @@
-def region_main_causes(df_filtered_causes, region):
-    # Determining main reasons that usually cause wildfires near a certain region
 
-    return region_main_causes
+def region_main_causes(df_filtered_causes, region):
+    # accounting for different input variations using title mapping
+    region = region.upper() # this alone accounts for inputs such as 'c'
+    title_mapping = { 
+        # meanwhile, title mapping will also account for full name inputs.
+        # this paired with the upper() function will account for any capitalization
+        # variation.
+        'CALGARY' : 'C',
+        'EDSON' : 'E',
+        'HIGH LEVEL' : 'H',
+        'GRANDE PRAIRIE' : 'G',
+        'LAC LA BICHE' : 'L',
+        'FORT MCMURRAY' : 'M',
+        'PEACE RIVER' : 'P',
+        'ROCKY' : 'R',
+        'SLAVE LAKE' : 'S',
+        'WHITECOURT' : 'W'
+    }
+    if region in title_mapping:
+        region = title_mapping[region]
+    
+    # Determining main industries that usually cause wildfires near a certain region
+
+    return region
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import pandas as pd
 from datetime import datetime
 import ImpactScore as imsc
 import FilterFireCause as ffc
+import RegionMainCauses as rmc
 import numpy as np
 
 excel_file = 'fp-historical-wildfire-data-2006-2021.xlsx'
@@ -12,13 +13,16 @@ df = pd.read_excel(excel_file, engine='openpyxl')
 
 #df['fire_cause'] = df.apply(fc.filter_fire_causes, axis=1)
 df_filtered_causes = ffc.filter_fire_causes(df)
-df_cause_industry = df_filtered_causes[0]
-df_cause_activity = df_filtered_causes[1]
-df_true_cause = df_filtered_causes[2]
-indices = list(range(1, 10, 1))
-print(df_cause_industry.iloc[indices])
-print(df_cause_activity.head(10))
-print(df_true_cause.head(10))
+#df_cause_industry = df_filtered_causes[0]
+#df_cause_activity = df_filtered_causes[1]
+#df_true_cause = df_filtered_causes[2]
+#indices = list(range(1, 10, 1))
+#print(df_cause_industry.iloc[indices])
+#print(df_cause_activity.head(10))
+#print(df_true_cause.head(10))
+
+df_region_main_cause = rmc.region_main_causes(df_filtered_causes, 'calgary')
+print(df_region_main_cause)
 #print(df_filtered_causes['general_cause_desc'].iloc[indices])
 #print(df_filtered_causes['activity_class'].iloc[indices])
 #indices = [90, 91, 92, 93, 94, 95, 96]
