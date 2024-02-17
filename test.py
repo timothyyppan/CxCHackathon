25527492cda46f56039f2c1c2fea209b371797a3
SunYeet <101126284+SunYeet@users.noreply.github.com>
2024-02-16 19:06:04 -0500

    fixed fire cause

--- test.py
+++ test.py
@@ -12,7 +12,14 @@ df = pd.read_excel(excel_file, engine='openpyxl')
 
 #df['fire_cause'] = df.apply(fc.filter_fire_causes, axis=1)
 df_filtered_causes = fc.filter_fire_causes(df)
-indices = list(range(90, 121, 1))
-print(df_filtered_causes['general_cause_desc'].iloc[indices])
+df_cause_industry = df_filtered_causes[0]
+df_cause_activity = df_filtered_causes[1]
+df_true_cause = df_filtered_causes[2]
+indices = list(range(1, 10, 1))
+print(df_cause_industry.iloc[indices])
+print(df_cause_activity.head(10))
+print(df_true_cause.head(10))
+#print(df_filtered_causes['general_cause_desc'].iloc[indices])
+#print(df_filtered_causes['activity_class'].iloc[indices])
 #indices = [90, 91, 92, 93, 94, 95, 96]
 #print(df_filtered_causes.iloc[indices])
