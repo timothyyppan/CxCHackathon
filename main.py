07bb7760904ccd5ae614a536ff9bccd85c4bb102
timothyyppan <timothypan676@gmail.com>
2024-02-18 21:42:08 -0500

    dfd

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ import FilterFireCause as ffc
 
 excel_file = 'fp-historical-wildfire-data-2006-2021.xlsx'
 
+#Opens the excel file as a dataframe using pandas
 df = pd.read_excel(excel_file, engine='openpyxl')
 
+#Removes any rows that are bad (Typos)
 df.drop(index=1291, inplace=True)
 df.drop(index=14316, inplace=True)
 #Should probably remove any entries that don't have a fire_start_date or we should replace it with the average detection time or smthn
+
+#Calculates the magnitude of the current_size column
 magnitude_all_current_sizes= math.sqrt((df['current_size']**2).sum())
 
 #Creates a column and fills it with the normalized current_size
