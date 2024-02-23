66a4430d4701f23d966e3d48a8ba80ed09835cf3
timothyyppan <timothypan676@gmail.com>
2024-02-23 15:59:36 -0500

    dbfdg

--- GetExposure.py
+++ GetExposure.py
@@ -1,7 +1,14 @@
 def get_exposure(df, region):
     df_filtered = df[df['fire_number'] == region]
-    current_size_sum = df_filtered['current_size'].sum()
+    AREAS = [25.34, 6.63, 10.70]
 
-    exposure = current_size_sum / (df['fire_year'].iloc[0] - df['fire_year'].iloc[-1])
-    
+    if region == "Peace River":
+        area = AREAS[0]
+    elif region == "Slave Lake":
+        area = AREAS[1]
+    else:
+        area = AREAS[2]
+
+    exposure = ((df_filtered['current_size'] / area).sum()) / df_filtered.shape[0]
+
     return exposure
